f9843d851c3401e7b90fce008ddfbb9d9ce6bda9
Edgar Erenberg <EdgarErenbergs@rambler.ru>
2023-10-03 14:42:25 +0300

    Added new tasks

---
+++ occurences.js
@@ -0,0 +1,43 @@
+/**
+ * Returns a count of occurrences of a character in a string
+ * @param {string} - str
+ * @param {string} - char
+ * @returns {number} - count of occurrences
+ */
+
+const getCountOfOccurences = (str, char) => {
+  // ------ BAD
+
+  const strArr = [...str];
+  const occurenceArr = [];
+
+  for (const letter of strArr) {
+    if (letter === char) {
+      occurenceArr.push(letter);
+    }
+  }
+
+  return occurenceArr.length;
+
+  // ------ SO-SO
+
+  let count = 0;
+
+  for (let i = 0; i <= str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+
+  // ------ THE BEST
+
+  return str.split(char).length - 1;
+};
+
+const res1 = getCountOfOccurences("Lalala lend", "l");
+const res2 = getCountOfOccurences("Hey! I think about it", "a");
+
+console.log(res1);
+console.log(res2);
